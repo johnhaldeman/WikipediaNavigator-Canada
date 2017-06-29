8cd00894cb735984c1068c002fa884b51620120c
John Haldeman <john.haldeman@infoinsightsllc.com>
2017-06-28 20:42:12 -0400

    Add main design of page

--- renderer.js
+++ renderer.js
@@ -1,13 +1,28 @@
-const BrowserWindow = require('electron').remote.BrowserWindow
-const path = require('path')
+const searchInPage = require('electron-in-page-search').default;
+const BrowserWindow = require('electron').remote.BrowserWindow;
+const path = require('path');
 
 const webview = document.querySelector('webview')
+let visitedURLs = [];
+let targetURL = 'https://en.wikipedia.org/wiki/Churchill,_Manitoba';
+let search;
+//let
 
 webview.addEventListener('will-navigate', (event) => {
   console.log(event.url);
+  visitedURLs.push(event.url);
+
+  if(event.url == targetURL){
+    console.log("You did it!");
+  }
 })
 
-  
+
 webview.addEventListener('dom-ready', () => {
-  //webview.openDevTools()
+  //webview.openDevTools();
+  search = searchInPage(webview);
 })
+
+document.getElementById('search-page-button').addEventListener('click', () => {
+  search.openSearchWindow();
+});
--- stripPage.js
+++ stripPage.js
@@ -5,7 +5,7 @@
   ready(
 	fireWhenReady
   );
-  
+
   function fireWhenReady(){
 	  console.log("Ready Now");
 	  const head = document.querySelector('#mw-head');
@@ -14,19 +14,19 @@
 	  panel.remove();
 	  const footer = document.querySelector('#footer');
 	  footer.remove();
-	  
-	  
+
+
 	  const pageBase = document.querySelector('#mw-page-base');
 	  pageBase.remove();
 	  const headBase = document.querySelector('#mw-head-base');
 	  headBase.remove();
-	  
-	  
+
+
 	  const body = document.querySelector('.mw-body');
 	  body.style.marginLeft = 0;
-	  
+
   }
-  
+
   function ready(fn) {
 	  if (document.readyState != 'loading'){
 		fn();
