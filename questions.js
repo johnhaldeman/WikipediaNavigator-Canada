95748414cc2c41b40aa9c67782236bc059e8915c
John Haldeman <john.haldeman@infoinsightsllc.com>
2017-07-07 22:18:39 -0400

    no message

---
+++ questions.js
@@ -0,0 +1,192 @@
+
+module.exports.questions =
+[
+{
+	"question": "This Shopping centre includes a waterpark, a habitat for four sea lions, and a hotel. It is the largest mall in North America. Starting at Canada find it's Wikipedia entry in less than four links.",
+
+	"target": "https://en.wikipedia.org/wiki/West_Edmonton_Mall",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "landmarks",
+
+	"click_limit": 4
+},
+{
+	"question": "This hotel near Quebec City is made of ice and takes approximately a month and a half for fifty workers to construct each year. Starting at Canada find it's Wikipedia entry in less than four links.",
+
+	"target": "https://en.wikipedia.org/wiki/Ice_Hotel_(Quebec)",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "landmarks",
+
+	"click_limit": 4
+},
+{
+	"question": "What is the Canadian Football League equivalent of America's NFL Superbowl? Starting at Canada find it's Wikipedia entry in less than six links.",
+
+	"target": "https://en.wikipedia.org/wiki/Grey_Cup",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "sport",
+
+	"click_limit": 6
+},
+{
+	"question": "Four Canadian companies that produce this beverage are Molson, Labatt, Sleeman, and Steam Whistle. Navigate to the beaverage's wikipedia article in eight links from the Canada page.",
+
+	"target": "https://en.wikipedia.org/wiki/Beer",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "economy",
+
+	"click_limit": 8
+},
+{
+	"question": "During the Second World War and up until 1965, this flag was used as the national flag of Canada. Navigate to it's page in four clicks starting at the Canada page.",
+
+	"target": "https://en.wikipedia.org/wiki/Canadian_Red_Ensign",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "history",
+
+	"click_limit": 4
+},
+{
+	"question": "During the War of 1812 between Britain and the US, this US Capital was burned including the the Presidential Mansion, but not before the US burned York (now Toronto) the then capital of Upper Canada. Navigate to the US Capital's page in 6 links from the Canada page.",
+
+	"target": "https://en.wikipedia.org/wiki/Washington,_D.C.",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "history",
+
+	"click_limit": 6
+},
+{
+	"question": "This, the largest French speaking city in Canada is the second-largest French Speaking city in the world after Paris. Navigate to it's page in one link from the Canada page.",
+
+	"target": "https://en.wikipedia.org/wiki/Montreal",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "history",
+
+	"click_limit": 1
+},
+{
+	"question": "John A. McDonald called this the chamber of \"sober second thought\". It's members are appointed by the Governor General on the advice of the Prime Minister. Starting at Canada find it's Wikipedia entry in less than three links.",
+
+	"target": "https://en.wikipedia.org/wiki/Senate_of_Canada",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "civics",
+
+	"click_limit": 3
+},
+{
+	"question": "This \"Polar Bear Capital of the World\" is the northernmost city in Canada that can be reached by rail. Navigate to this city's Wikipedia page from the Canada Wikipedia page in less than four links.",
+
+	"target": "https://en.wikipedia.org/wiki/Churchill,_Manitoba",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "geography",
+
+	"click_limit": 4
+},
+{
+	"question": "This Edmonton born \"Back to the Future\" star was awarded the Order of Canada in 2010. Navigate to his Wikipedia page in eight links from the Canada page.",
+
+	"target": "https://en.wikipedia.org/wiki/Michael_J._Fox",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "culture",
+
+	"click_limit": 10
+},
+{
+	"question": "(At the time of this question being written - July, 2017) this natural disaster was the most expensive ever for Canada in terms of insurance losses but there were no direct fatalities and zero injuries. Navigate to the Wikipedia page for this disaster in from the Canada Wikipedia page in less than six links.",
+
+	"target": "https://en.wikipedia.org/wiki/2016_Fort_McMurray_Wildfire",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "history",
+
+	"click_limit": 6
+},
+{
+	"question": "This Island, part of Point Pelee National Park, is the southernmost point of land in Canada. Find this Island's Wikipedia entry in ten links or under starting at Canada.",
+
+	"target": "https://en.wikipedia.org/wiki/Middle_Island_(Lake_Erie)",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "geography",
+
+	"click_limit": 10
+},
+{
+	"question": "This Métis leader of the Red River Rebellion was a key figure in negotiating the terms that established Manitoba as a province. Navigate to that man's Wikipedia entry from the Canada entry in 4 links",
+
+	"target": "https://en.wikipedia.org/wiki/Louis_Riel",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "history",
+
+	"click_limit": 4
+},
+{
+	"question": "This company, based out of the largest city in Saskatchewan, is the world's largest producer of a primary crop nutrient used in fertilizer. Navigate to that company's Wikipedia entry from the Canada entry in 5 links",
+
+	"target": "https://en.wikipedia.org/wiki/PotashCorp",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "economy",
+
+	"click_limit": 4
+},
+{
+	"question": "This was the host city of the 2010 Winter Olympic Games. Navigate to that city's Wikipedia entry from the Canada entry in 3 links",
+
+	"target": "https://en.wikipedia.org/wiki/Vancouver",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "sport",
+
+	"click_limit": 3
+},
+{
+	"question": "Canada holds the distinction of participating in the first ever international cricket match and the longest running international sporting competition. Navigate to the location in Manhattan of this first match from the Canada entry in 10 links",
+
+	"target": "https://en.wikipedia.org/wiki/St_George%27s_Cricket_Club",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "sport",
+
+	"click_limit": 10
+},
+{
+	"question": "This Canadian Premiere's government introduced the first single-payer, universal health care program. Navigate to his Wikipedia page in six links from the Canada page.",
+
+	"target": "https://en.wikipedia.org/wiki/Tommy_Douglas",
+
+	"start": "https://en.wikipedia.org/wiki/Canada",
+
+	"category": "culture",
+
+	"click_limit": 6
+}
+
+]
--- renderer.js
+++ renderer.js
@@ -1,28 +1,164 @@
 const searchInPage = require('electron-in-page-search').default;
 const BrowserWindow = require('electron').remote.BrowserWindow;
 const path = require('path');
+const questions = require('./questions').questions;
+const ranks = require('./ranks').ranks;
 
 const webview = document.querySelector('webview')
-let visitedURLs = [];
+let visitedURLs = ['https://en.wikipedia.org/wiki/Canada'];
 let targetURL = 'https://en.wikipedia.org/wiki/Churchill,_Manitoba';
+let currentQuestion = "";
+let maxURLs = 4;
+let currentIndex = 0;
+let rankStep = 1;
 let search;
-//let
+
+function loadQuestion(index){
+  currentQuestion = questions[index].question;
+  targetURL = questions[index].target;
+  visitedURLs = [questions[index].start];
+  webview.src = questions[index].start;
+  maxURLs = questions[index].click_limit;
+}
+
+loadQuestion(currentIndex);
+let questText = document.getElementById('question-text');
+questText.textContent = currentQuestion;
+
+function drawHistory(){
+  const panel = document.querySelector('#history-panel');
+  removeAllChildren(panel);
+  let html = '';
+  for(let i = 0; i < visitedURLs.length; i++){
+    html += `<li role="presentation"><a href="#" link="${visitedURLs[i]}">${visitedURLs[i]}</a></li>`;
+  }
+  panel.innerHTML = html;
+  panel.lastChild.setAttribute("class", "active");
+  for(let i = 0; i < panel.children.length; i++){
+    addHistoryLinkListener(panel.children[i], i);
+  }
+}
+
+function historyNavigate(elem, elemNum){
+  return function(){
+    webview.src = elem.textContent;
+    visitedURLs.splice(elemNum + 1, visitedURLs.length - elemNum);
+    refresh();
+  }
+}
+
+function addHistoryLinkListener(element, elemNum){
+  element.addEventListener("click", historyNavigate(element, elemNum));
+}
+
+
+function refresh(){
+   drawHistory();
+   drawRank();
+}
+
+function drawRank(){
+  let rankIndex = Math.floor(currentIndex / rankStep);
+  let rank = ranks[rankIndex];
+
+  document.getElementById('solved-puzzles').textContent = currentIndex + 1;
+  document.getElementById('total-puzzles').textContent = questions.length;
+  document.getElementById('rank-name').textContent = rank.name;
+  document.getElementById('rank-img').setAttribute('src', rank.image);
+
+}
+
+function removeAllChildren(element){
+  while (element.firstChild) {
+      element.removeChild(element.firstChild);
+  }
+}
+
+refresh();
 
 webview.addEventListener('will-navigate', (event) => {
-  console.log(event.url);
+
+  if(maxURLs == visitedURLs.length){
+    console.log('oops, reached max');
+    webview.src = visitedURLs[visitedURLs.length - 1];
+    openOverlay("fail");
+    return;
+  }
   visitedURLs.push(event.url);
 
-  if(event.url == targetURL){
-    console.log("You did it!");
-  }
+  refresh();
 })
-
 
 webview.addEventListener('dom-ready', () => {
   //webview.openDevTools();
   search = searchInPage(webview);
+
+  if(webview.src.toUpperCase() == targetURL.toUpperCase()){
+    openOverlay("success");
+    refresh();
+    let qPanel = document.getElementById('question-panel');
+    qPanel.classList.remove("panel-danger");
+    qPanel.classList.add("panel-success");
+
+    let challText = document.getElementById('challenge-text');
+    challText.textContent = "Challenge Complete: "
+
+    document.getElementById('next-challenge').style.display = 'block';
+
+    maxURLs = 100;
+  }
 })
 
 document.getElementById('search-page-button').addEventListener('click', () => {
   search.openSearchWindow();
 });
+
+document.getElementById('search-page-button').addEventListener('click', () => {
+  search.openSearchWindow();
+});
+
+document.getElementById('overlay').addEventListener('click', () => {
+    closeOverlay();
+});
+
+document.getElementById('next-challenge').addEventListener('click', () => {
+  currentIndex++;
+  loadQuestion(currentIndex);
+
+  let qPanel = document.getElementById('question-panel');
+  qPanel.classList.remove("panel-success");
+  qPanel.classList.add("panel-danger");
+
+  let challText = document.getElementById('challenge-text');
+  challText.textContent = "Your Next Challenge: "
+  let questText = document.getElementById('question-text');
+  questText.textContent = currentQuestion;
+
+  document.getElementById('next-challenge').style.display = 'none';
+
+  refresh();
+});
+
+function openOverlay(type) {
+  document.getElementById("fail-text").style.display = "none";
+  document.getElementById("success-text").style.display = "none";
+  let overlay = document.getElementById("overlay");
+  overlay.style.height = "100%";
+  if(type == 'fail'){
+      overlay.style.backgroundColor = "rgba(169, 68, 66, 0.9)";
+      document.getElementById("fail-text").style.display = "block";
+  }
+  else if(type == 'success'){
+      overlay.style.backgroundColor = "rgba(223, 240, 216, 0.95)";
+      let answerText = document.getElementById("answer-text");
+      answerText.textContent = webview.getTitle().replace(" - Wikipedia", "");;
+      document.getElementById("success-text").style.display = "block";
+  }
+  else
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+
+}
+
+function closeOverlay() {
+    document.getElementById("overlay").style.height = "0%";
+}
